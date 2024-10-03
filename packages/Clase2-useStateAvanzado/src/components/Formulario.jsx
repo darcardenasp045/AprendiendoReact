3cc95ff8eaef3b0a21cdfa4d5f1ee16b1962b3fd
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2024-10-03 10:29:10 -0500

    Add the fist excercise of the second class

---
+++ packages/Clase2-useStateAvanzado/src/components/Formulario.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function FormularioBasico () {
+
+    const [nombre, setNombre] = useState('')
+    const [apellido, serApellido] = useState('')
+
+    const handleName = (e) =>{
+        e.preventDefault()
+        alert(`Hola ${nombre} ${apellido}`)
+
+        
+    }
+
+    return (
+        <div
+            className="w-1/2 mx-auto m-4 p-4 bg-gray-700 rounded-lg"
+        >
+            <h2>FORMUALRIO BASICO CON USESTATE</h2>
+            <form 
+                onSubmit={handleName}
+                className="flex flex-col p-5"
+            >
+                <label>Nombre:</label>
+                <input 
+                    type="text"
+                    value={nombre}
+                    className=""
+                    onChange={(e)=>setNombre(e.target.value)}
+                />
+                <label> Apellido:</label>
+                <input 
+                    type="text"
+                    value={apellido}
+                    onChange={(e) => serApellido(e.target.value)}
+                />
+            
+            <button type="submit">enviar</button>
+            </form>
+        
+        </div>
+        
+    )
+}
+
+export default FormularioBasico
